933b4bad35fe317dd08a0b3f0f915492552f9745
Exequiel <104279077+DocBlackWolf@users.noreply.github.com>
2024-08-26 17:49:54 -0300

    first commit

---
+++ Assets/Scrips/player/SC_Jump.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Jump : MonoBehaviour
+{
+    [Header("Configuracion")]
+    [SerializeField] private float fuerzaSalto = 5f;
+
+    private bool puedoSaltar = true;
+    private bool saltando = false;
+
+    private Rigidbody2D miRigidbody2D;
+
+    private void OnEnable()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space) && puedoSaltar)
+        {
+            puedoSaltar = false;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (!puedoSaltar && !saltando)
+        {
+            miRigidbody2D.AddForce(Vector2.up * fuerzaSalto, ForceMode2D.Impulse);
+            saltando = true;
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        puedoSaltar = true;
+        saltando = false;
+    }
+
+}
---
+++ Assets/Scrips/player/SC_Movement.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SC_Movement : MonoBehaviour
+{
+    [Header("Configuracion")]
+    [SerializeField] float velocidad = 5f;
+
+    private float moverHorizontal;
+    private Vector2 direccion;
+
+    private Rigidbody2D miRigidbody2D;
+
+    private void OnEnable()
+    {
+        miRigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    private void Update()
+    {
+        moverHorizontal = Input.GetAxis("Horizontal");
+        direccion = new Vector2(moverHorizontal, 0f);
+    }
+    private void FixedUpdate()
+    {
+        miRigidbody2D.AddForce(direccion * velocidad);
+    }
+}
